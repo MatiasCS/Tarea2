50cbf1d887e5bcc57064eb17d44c55456538fe7f
Matias <matias.campos@alumnos.usm.cl>
2014-05-24 01:44:53 -0400

    Creacion del servidor TCP que corre en mi localhost Por ahora solo recibe mensajes y los guarda en el archivo Chat.txt

---
+++ TCPServer/src/tcpserver/TCPServer.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tcpserver;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Matias
+ */
+public class TCPServer implements Runnable{
+
+    static final int puerto = 6789;
+    Socket conexion;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try {
+            // TODO code application logic here
+            //Creacion del servidor y espera de clientes            
+            ServerSocket servidor = new ServerSocket(puerto);
+            while(true){
+                TCPServer cliente = new TCPServer(servidor.accept());
+                Thread hebra = new Thread(cliente);
+                hebra.start();  
+              }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public TCPServer(Socket conexion){
+        this.conexion = conexion;
+    }
+
+    @Override
+    public void run() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        BufferedReader inClienteTCP;
+        System.out.println("Hola acepte conexion");
+        try {
+            inClienteTCP = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+            String mensaje = inClienteTCP.readLine();
+            System.out.println(mensaje);
+            File fichero;
+            fichero = new File("Chat.txt");
+            
+            FileWriter escritor=new FileWriter(fichero,true);
+            BufferedWriter buffescritor=new BufferedWriter(escritor);
+            PrintWriter escritor_final= new PrintWriter(buffescritor);
+                    
+            escritor_final.append(mensaje+"\r\n");
+            escritor_final.close();
+            buffescritor.close();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+}

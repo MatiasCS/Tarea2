9b823bad17f31cdaac85b9194b54be0bedf3fb9e
MatiasCS <matias.campos@alumnos.usm.cl>
2014-06-01 13:04:21 -0400

    Corrección de errores en el código
    Agregaron los imports que faltaban

--- TCPServer/src/tcpserver/TCPServer.java
+++ TCPServer/src/tcpserver/TCPServer.java
@@ -6,12 +6,15 @@
 
 package tcpserver;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
